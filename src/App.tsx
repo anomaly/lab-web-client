9760e74ff2ad4dcd00faedd474fa882d43c3ed31
Dev Mukherjee <devraj@gmail.com>
2022-07-20 10:40:14 +0930

    feat: adds basic navigation to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import { LabsApiClient } from 'api/otc';
 // TODO: see if we can import this as a module
 import { LabsApi } from 'api/ar/src';
+import AnomalyLogo from 'assets/icon-anomaly.png';
 
 function App() {
 
@@ -27,10 +29,27 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold">Anomaly Labs</h1>
-      <button onClick={otcCallMe}>OTC Client</button>
-      <button onClick={arCallme}>AR Client</button>
+    <div className="flex flex-col justify-center w-screen h-screen p-20 text-black app">
+      <img
+        src={AnomalyLogo}
+        width={40}
+        height={40}/>
+      <h1 className="my-4 text-3xl font-bold">Anomaly Labs</h1>
+      <nav>
+        <ul className="flex flex-col">
+          <li className="text-blue-700 underline">
+            <RouterLink to="/auth/otp">
+              Sign in with One Time Password
+            </RouterLink>
+          </li>
+          <li className="text-blue-700 underline">
+            <RouterLink to="/auth/login">
+              Sign in with Email &amp; Password
+            </RouterLink>
+          </li>
+
+        </ul>
+      </nav>
     </div>
   );
 }
