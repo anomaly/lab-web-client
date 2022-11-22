1925d43f769dbf181cc207a951ef68d7f9e46418
Dev Mukherjee <devraj@gmail.com>
2022-11-22 13:46:32 +1030

    feat: adds axios interceptor to redirect to login page

--- src/routers/auth/login.tsx
+++ src/routers/auth/login.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import { redirect, Form } from 'react-router-dom';
+import {
+    QueryClient,
+} from 'react-query';
+
+import { 
+    loginForAuthToken, useLoginForAuthToken,
+} from 'api/auth/auth';
+import { Helmet } from 'react-helmet';
+
+export const action = (queryClient: QueryClient) => 
+async({ request } : any) => {
+    const formData = await request.formData();
+    const loginInfo = Object.fromEntries(formData);
+    await loginForAuthToken({
+        username: loginInfo.username,
+        password: loginInfo.password,
+    }).then((response) => {
+        queryClient.setQueryData('access_token', response.data?.access_token);
+    }).catch((error) => {
+        console.log(error);
+    });
+    return redirect('/admin/users');
+};
 
 function Login() {
+
     return(
-    <div>
-        <h1>Login</h1>
-    </div>);
+        <div>
+            <Helmet>
+                <title>Login</title>
+            </Helmet>
+            <Form method="post" id="login-user">
+                <span>Username</span>
+                <input
+                placeholder="user@domain.com"
+                aria-label="Username"
+                type="text"
+                name="username"
+                />
+                <span>Password</span>
+                <input
+                type="password"
+                placeholder=""
+                aria-label="Password"
+                name="password"
+                />
+                <button type="submit">Save</button>
+            </Form>
+        </div>
+    );
 }
 
 export default Login;
